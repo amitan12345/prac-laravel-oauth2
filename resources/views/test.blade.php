8cc3d3c0bd0be3ee34b234472bb353b196a45360
amitan12345 <03_habitat.shelter@icloud.com>
2026-02-22 17:19:26 +0900

    chore: 不必要なタグを削除した

--- resources/views/test.blade.php
+++ resources/views/test.blade.php
@@ -4,8 +4,6 @@
         @vite(['resources/css/app.css', 'resources/js/app.js'])
     </head>
     <body>
-        <div id="app">
-            <counter-button></counter-button>
-        </div>
+        <div id="app"></div>
     </body>
 </html>
